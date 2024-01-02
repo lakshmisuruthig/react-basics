729240e53e69cc3836d71b11ddc7a2e4e7829afc
Lakshmi Suruthi Ganesan <lakshmisuruthig@gmail.com>
2024-01-02 13:37:27 +0530

    Added Dynamic Value for header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./Header";
+
 function App() {
   return (
     <div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,15 @@
+const reactDescriptions = ["Fundamental", "Core", "Crucial"];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
 function Header() {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Styled Atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {reactDescriptions[getRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
   );
